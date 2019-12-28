eedadc1e83b2bbab0378cdc7b3ee89c7e4ab09ca
huti26 <31436976+huti26@users.noreply.github.com>
2019-12-28 05:14:27 +0100

    added basic styling

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,13 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
+function calc(){
+    var tier = parseFloat(document.getElementById('tier').value);
+    var level = parseFloat(document.getElementById('level').value);
+    var gold = parseFloat(document.getElementById('gold').value);
+    var champ_picked = parseFloat(document.getElementById('champ_picked').value);
+    var other_champs_picked = parseFloat(document.getElementById('other_champs_picked').value);
+
+
+    
+    document.getElementById('result').value = tier + level;
+
+    
+}
