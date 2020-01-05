5fb04c3725fc537cfee77e1f86f608e2eeec6a6e
huti26 <31436976+huti26@users.noreply.github.com>
2020-01-05 20:49:04 +0100

    changed dark mode switch logic

--- js/dark-mode-switch.js
+++ js/dark-mode-switch.js
@@ -8,9 +8,33 @@ $(function () {
       document.body.setAttribute("data-theme", "dark");
       localStorage.setItem("theme", "dark");
     }
-    localStorage.setItem("theme", "xd");
-    console.log(localStorage.getItem(theme));
+    //console.log(localStorage.getItem("theme"));
   })
 })
 
 
+// on page load
+document.addEventListener('DOMContentLoaded', (event) => {
+  var theme = localStorage.getItem("theme")
+  //console.log(theme);
+  //console.log(darkSwitch.checked);
+
+  // checked = light theme is current theme
+  if (theme == "dark") {
+    document.body.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    if (darkSwitch.checked) {
+      document.getElementById('darkSwitch').click();
+    }
+  }
+  else {
+    document.body.removeAttribute("data-theme");
+    localStorage.setItem("theme", "light");
+    if (!darkSwitch.checked) {
+      document.getElementById('darkSwitch').click();
+    }
+  }
+
+
+
+})
