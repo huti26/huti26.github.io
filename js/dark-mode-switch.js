65a4966d5f09fe4f799a253b8e02bcc76601fd75
huti26 <31436976+huti26@users.noreply.github.com>
2020-01-06 12:55:29 +0100

    change button theming

--- js/dark-mode-switch.js
+++ js/dark-mode-switch.js
@@ -1,12 +1,20 @@
 // change theme on button hit
 $(function () {
   $('#darkSwitch').change(function () {
+
+    // darkSwitch checked = light theme
     if (darkSwitch.checked) {
       document.body.removeAttribute("data-theme");
       localStorage.setItem("theme", "light");
+
+      document.getElementById("tft-calc-btn").classList.remove("btn-light");
+      document.getElementById("tft-calc-btn").classList.add("btn-dark");
     } else {
       document.body.setAttribute("data-theme", "dark");
       localStorage.setItem("theme", "dark");
+
+      document.getElementById("tft-calc-btn").classList.remove("btn-dark");
+      document.getElementById("tft-calc-btn").classList.add("btn-light");
     }
     //console.log(localStorage.getItem("theme"));
   })
@@ -20,19 +28,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
   //console.log(darkSwitch.checked);
 
   // checked = light theme is current theme
-  if (theme == "dark") {
-    document.body.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-    if (darkSwitch.checked) {
-      document.getElementById('darkSwitch').click();
-    }
-  }
-  else {
+  if (theme != "dark") {
     document.body.removeAttribute("data-theme");
     localStorage.setItem("theme", "light");
+
+    document.getElementById("tft-calc-btn").classList.remove("btn-light");
+    document.getElementById("tft-calc-btn").classList.add("btn-dark");
+
     if (!darkSwitch.checked) {
       document.getElementById('darkSwitch').click();
     }
+  }
+
+  else {
+    document.body.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+
+    document.getElementById("tft-calc-btn").classList.remove("btn-dark");
+    document.getElementById("tft-calc-btn").classList.add("btn-light");
+
+    if (darkSwitch.checked) {
+      document.getElementById('darkSwitch').click();
+    }
+
   }
 
 
